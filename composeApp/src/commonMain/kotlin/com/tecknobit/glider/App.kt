21566b2bb9548ccc2164c67841116366b8d5ed55
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-03-30 20:48:56 +0200

    Added and fixed last details

--- composeApp/src/commonMain/kotlin/com/tecknobit/glider/App.kt
+++ composeApp/src/commonMain/kotlin/com/tecknobit/glider/App.kt
@@ -144,7 +144,7 @@ private fun InitAmetista() {
             serverSecret = AmetistaConfig.SERVER_SECRET!!,
             applicationId = AmetistaConfig.APPLICATION_IDENTIFIER!!,
             bypassSslValidation = AmetistaConfig.BYPASS_SSL_VALIDATION,
-            debugMode = true // TODO: TO SET ON FALSE
+            debugMode = false
         )
     }
 }
